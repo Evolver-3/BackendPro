8c83d4b3b142d58e553828aa2efa432de47ff469
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-10 00:18:28 +0530

    first route added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 
+// import dotenv from 'dotenv'
+// import {app} from './app.js'
+
+// import connectDB from './db/db.js'
+
+// dotenv.config({
+//   path:'./.env'
+// })
+
+// connectDB().
+// then(()=>{
+//   app.listen(process.env.PORT || 8000,()=>{
+//     console.log(`Server is running on port ${process.env.PORT}`)
+//   })
+// })
+// .catch((err)=>{
+//   console.log(`MONGO DB connection failed !!`, err)
+// })
+
 import dotenv from 'dotenv'
 import {app} from './app.js'
 
 import connectDB from './db/db.js'
 
 dotenv.config({
-  path:'./.env'
+  path:"./.env"
 })
 
-connectDB().
-then(()=>{
+connectDB().then(()=>{
   app.listen(process.env.PORT || 8000,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
   })
-})
-.catch((err)=>{
-  console.log(`MONGO DB connection failed !!`, err)
+
+}).catch((error)=>{
+  console.log(`MONGO DB connection failed !!`,error)
 })
