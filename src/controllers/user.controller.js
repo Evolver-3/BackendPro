fb60f57ab98574b33a06a9cf671f68296929f050
Ashu Singh <Ashusingh2201@gmail.com>
2026-03-11 21:39:10 +0530

    loginUser controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import {User} from '../models/user.model.js'
 import { uploadToCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 
+const generateAccessAndRefreshToken=async(userId)=>{
+  try{
+    const user =await User.findById(userId)
+
+    const accessToken=user.generateAccessToken()
+    const refreshToken=user.generateRefreshToken()
+
+    user.refreshToken=refreshToken
+    await user.save({validateBeforeSave:false})
+
+    return {accessToken,refreshToken}
+
+  }catch(error){
+    throw new ApiError("Something went wrong while generating access and refresh token!!", 500)
+  }
+}
+
 
 const registerUser=asyncHandler(async(req,res)=>{
   
@@ -68,6 +85,42 @@ return res.status(201).json(
   
 })
 
+const loginUser=asyncHandler(async(req,res)=>{
+  
+const {email, username, password}=req.body
+if(!username || !email){
+  throw new ApiError("Email and username are required !!",400)
+}
+
+const user =await User.findOne({
+  $or:[{email},{username}]
+})
+
+if(!user){
+  throw new ApiError("Invalid credentials !! ", 404)
+}
+
+const isPasswordCorrect=await user.isPasswordCorrect(password)
+
+if(!isPasswordCorrect){
+  throw new ApiError("Password incorrect",400)
+}
+
+const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
+
+const loggedInUser=await User.findById(user._id).select("-password -refreshToken")
+
+const options={
+  httpOnly:true,
+  secure:true
+}
+
+return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",refreshToken,options).json(
+  new ApiResponse(200,{
+    user:loggedInUser,accessToken,refreshToken
+  },"User logged in successfully !!")
+)
+})
 
 
-export {registerUser}
+export {registerUser,loginUser}
